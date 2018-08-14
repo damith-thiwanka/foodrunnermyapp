889107ddf1a234b0c7558b2a037bc8a86063fb8f
Damith Thiwanka <damith.c@foodrunner.asia>
2018-08-14 13:51:36 +0530

    init

---
+++ app/src/main/java/lk/electfast/thiwanka/foodrunner_myapp/SplashActivity.java
@@ -0,0 +1,27 @@
+package lk.electfast.thiwanka.foodrunner_myapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // setContentView(R.layout.activity_splash);
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent intent = new Intent(SplashActivity.this , HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
+
+    }
+}
